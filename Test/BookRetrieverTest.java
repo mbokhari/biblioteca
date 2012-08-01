d867fc02e2e7cdda4ddcc2e78948743c2d53832c
Mashell <mashell.bokhari@gmail.com>
2012-08-01 15:10:43 +0100

    retrieve test + code

---
+++ Test/BookRetrieverTest.java
@@ -0,0 +1,24 @@
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class BookRetrieverTest {
+    @Test
+    public void testBookRetriever() throws Exception {
+        //Given
+        ArrayList<Book> books = new ArrayList<Book>();
+        Book aBook = new Book("Harry Potter");
+        books.add(aBook);
+        Library library = new Library(books);
+        BookRetriever bookRetriever= new BookRetriever(library);
+
+        //When
+        Book retrievedBook = bookRetriever.retrieveBooks("Harry Potter");
+
+        //Then
+        assertThat(aBook, is(retrievedBook));
+    }
+}
